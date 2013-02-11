36dfd17354ee81f0961b148b262b81d0d9c51696
Adam Parrish <aparrish@neosavvy.com>
2013-02-10 23:17:54 -0500

    Now we have reverse/forward focus working.

--- app/js/services.js
+++ app/js/services.js
@@ -25,6 +25,7 @@ services.factory("focusManager", function () {
 
             if( !this.hasGroup(focusGroup) ) {
                 focusGroups[focusGroup] = [];
+                currentFocusItemIndex[focusGroup] = 0;
             }
 
         },
@@ -33,12 +34,49 @@ services.factory("focusManager", function () {
             var lengthOfFocusGroup = focusGroups[focusGroup].length;
             focusGroups[focusGroup][lengthOfFocusGroup] = element;
 
+            if( lengthOfFocusGroup == 0 ) {
+                element.addClass('focused');
+            }
+
         },
         forwardFocus: function( focusGroup ) {
             console.log("forwarding focus");
+
+            if( !currentFocusItemIndex.hasOwnProperty(focusGroup) ) {
+                currentFocusItemIndex[focusGroup] = 0;
+            }
+
+            focusGroups[focusGroup][currentFocusItemIndex[focusGroup]].removeClass('focused');
+
+            currentFocusItemIndex[focusGroup] = currentFocusItemIndex[focusGroup] + 1;
+
+            if( currentFocusItemIndex[focusGroup] >=  focusGroups[focusGroup].length) {
+                currentFocusItemIndex[focusGroup] = 0;
+            }
+
+            console.log("currentFocusIndex: " + currentFocusItemIndex[focusGroup]);
+
+            focusGroups[focusGroup][currentFocusItemIndex[focusGroup]].addClass('focused');
+
         },
         reverseFocus: function( focusGroup ) {
             console.log("reversing focus");
+
+            if( !currentFocusItemIndex.hasOwnProperty(focusGroup) ) {
+                currentFocusItemIndex[focusGroup] = focusGroups[focusGroup].length;
+            }
+
+            focusGroups[focusGroup][currentFocusItemIndex[focusGroup]].removeClass('focused');
+
+            currentFocusItemIndex[focusGroup] = currentFocusItemIndex[focusGroup] - 1;
+
+            if( currentFocusItemIndex[focusGroup] < 0 ) {
+                currentFocusItemIndex[focusGroup] = focusGroups[focusGroup].length - 1;
+            }
+
+            console.log("currentFocusIndex: " + currentFocusItemIndex[focusGroup]);
+
+            focusGroups[focusGroup][currentFocusItemIndex[focusGroup]].addClass('focused');
         },
         dumpGroups: function() {
 
